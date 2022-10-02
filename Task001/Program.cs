b753269fb25638bef6909ef88583100ccd24ffa8
NATALIA <natalia.dmitrievna@icloud.com>
2022-10-02 16:12:33 +0300

    Добавила решение задачи 25

---
+++ Task001/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+
+Ограничения:
+- Нельзя использовать класс Math
+- Должна быть отдельная функция Power, которая возвращает результат
+- Внутри функций НЕЛЬЗЯ использовать класс Console. Только в основном блоке кода
+*/
+
+Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int Number;
+
+int Power(int numberA, int numberB)
+{
+  int result = 1;
+
+  for(int i=1; i <= numberB; i++)
+  {
+    
+    result = result * numberA;
+  }
+  return result;
+}
+
+Number = Power(numberA, numberB);
+Console.WriteLine($"{numberA}^{numberB} = {Number}");
